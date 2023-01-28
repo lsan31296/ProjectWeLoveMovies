4f36e07681c01adb5a38b196c775ba231e943ad2
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-01-28 18:45:16 -0500

    Complete 'GET /movies?is_showing=true'. Updated error in migration file for createMoviesTable.

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,7 +2,9 @@ const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res, next) {
-    const data = await moviesService.list();
+    const is_showing = req.query.is_showing;
+    //console.log(is_showing);
+    const data = is_showing === "true" ? await moviesService.listShowingMovies() : await moviesService.list();
     res.json({ data });
 }
 
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -5,6 +5,15 @@ function list() {
     return knex("movies").select("movie_id", "title", "runtime_in_minutes", "rating", "description", "image_url");
 }
 
+function listShowingMovies () {
+    return knex("movies as m")
+        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+        .select("m.movie_id", "m.title", "m.runtime_in_minutes", "m.rating", "m.description", "m.image_url")
+        .where({ "mt.is_showing": true })
+        .groupBy("m.movie_id");
+}
+
 module.exports = {
     list,
+    listShowingMovies,
 };
